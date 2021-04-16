859e6201603b1c0bd04a919d6a186953db6ef444
Michael <michael.alvarado1@upr.edu>
2021-04-15 21:45:25 -0400

    init commit

---
+++ Assets/Scripts/ImageClient.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+
+/// <summary>
+/// This Client inheritated class acts like Client using UI elements like buttons and input fields.
+/// </summary>
+public class ImageClient : Client
+{
+    [Header("UI References")]
+    [SerializeField] private Button m_StartClientButton = null;
+    [SerializeField] private Button m_SendImageToServerButton = null;
+    [SerializeField] private Button m_SendCloseButton = null;
+    [SerializeField] private Text m_ClientLogger = null;
+    [SerializeField] public string imagePath = null;
+
+    //Set UI interactable properties
+    private void Awake()
+    {
+        //Start Client
+        m_StartClientButton.onClick.AddListener(base.StartClient);
+
+        //Send to Image Server
+        m_SendImageToServerButton.interactable = false;
+        m_SendImageToServerButton.onClick.AddListener(SendImageToServer);
+
+        //SendClose
+        m_SendCloseButton.interactable = false;
+        m_SendCloseButton.onClick.AddListener(SendCloseToServer);
+
+        //Populate Client delegates
+        OnClientStarted = () =>
+        {
+            //Set UI interactable properties        
+            m_SendCloseButton.interactable = true;
+            m_SendImageToServerButton.interactable = true;
+            m_StartClientButton.interactable = false;
+        };
+
+        OnClientClosed = () =>
+        {
+            //Set UI interactable properties        
+            m_StartClientButton.interactable = true;
+            m_SendImageToServerButton.interactable = false;
+            m_SendCloseButton.interactable = false;
+        };
+    }
+
+    public void SendImageToServer()
+    {
+        base.SendImageToServer(LoadImage(imagePath));
+    }
+
+    private byte[] LoadImage(string FilePath)
+    {
+        // Load a PNG or JPG file from disk to a Texture2D
+        // Returns null if load fails
+        byte[] FileData;
+
+        if (File.Exists(FilePath))
+        {
+            FileData = File.ReadAllBytes(FilePath);
+            return FileData;                 // If data = readable -> return texture
+        }
+        Debug.Log("Image Not Found");
+        return null;                     // Return null if load failed
+    }
+
+    private void SendCloseToServer()
+    {
+        base.SendMessageToServer("Close");
+        //Set UI interactable properties        
+        m_SendCloseButton.interactable = false;
+    }
+
+    //Custom Client Log
+    #region ClientLog
+    protected override void ClientLog(string msg, Color color)
+    {
+        base.ClientLog(msg, color);
+        m_ClientLogger.text += '\n' + "<color=#" + ColorUtility.ToHtmlStringRGBA(color) + ">- " + msg + "</color>";
+    }
+    protected override void ClientLog(string msg)
+    {
+        base.ClientLog(msg);
+        m_ClientLogger.text += '\n' + "- " + msg;
+    }
+    #endregion
+}
---
+++ Assets/Scripts/ImageLoader.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ImageLoader : MonoBehaviour
+{
+    [SerializeField] RawImage image;
+    [SerializeField] ImageClient client;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Makes a Tecture2D from the picture in FilePath.
+    /// Might throw error if image is not found.
+    /// </summary>
+    /// <param name="FilePath">File address path to the image to load</param>
+    /// <returns>Texture2D with the image on FilePath</returns>
+    public Texture2D LoadTexture(string FilePath)
+    {
+        // Load a PNG or JPG file from disk to a Texture2D
+        // Returns null if load fails
+        Texture2D Tex2D;
+        byte[] FileData;
+
+        if (File.Exists(FilePath))
+        {
+            FileData = File.ReadAllBytes(FilePath);
+            Tex2D = new Texture2D(1, 1);           // Create new "empty" texture
+            if (Tex2D.LoadImage(FileData))
+            {           // Load the imagedata into the texture (size is set automatically)
+                image.texture = Tex2D;         //Display on UI
+                client.imagePath = FilePath;
+                return Tex2D;                 // If data = readable -> return texture
+            }
+        }
+        return null;                     // Return null if load failed
+    }
+}
